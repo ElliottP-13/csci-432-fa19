28981bea9af448fa37fd9cdfd3aaaa86e8e56aeb
ElliottP-13 <pryorelliott@gmail.com>
2019-11-18 20:23:47 -0700

    Cleaned lineBreaker.py

--- lineBreaker.py
+++ lineBreaker.py
@@ -20,18 +20,6 @@ def findFirst(s, start):
 # This will return absolute paths
 file_list = [f for f in iglob('**/*', recursive=True) if os.path.isfile(f)]
 
-x = "ab,cdef.ghij,klmnopqrst,uwxyz"
-print(findFirst(x, 0))
-print(findFirst(x, 5))
-print(findFirst(x, 14))
-
-fileTest = open('demo.tex', 'r')
-arr = []
-for line in fileTest:
-    for i in range(0, len(line), 80):
-        arr.append(line[i: i + 80].strip())
-print(arr)
-
 for path in file_list:
     if path.endswith('.tex'):
         print(path)
@@ -42,7 +30,6 @@ for path in file_list:
             lastIndx = 0
             while run:
                 indx = findFirst(line, lastIndx + 80)
-                print(str(indx-lastIndx))
                 arr.append(line[lastIndx:indx])
                 lastIndx = indx + 1
 
@@ -52,24 +39,5 @@ for path in file_list:
 
         fileWrite = open(path, 'w')
         writeStr = "\n".join(arr)
-        print(writeStr)
         fileWrite.write(writeStr)
         fileWrite.close()
-
-
-# for path in file_list:
-#     if path.endswith(".tex"):
-#         print()
-#         print(path)
-#         path.strip()
-#         infile = open(path, 'r')
-#         outfile = open(path, 'w')
-#         writeME = ""
-#         for line in infile:
-#             print(line)
-#             if len(line) > 80:
-#                 writeME = writeME + ('\n'.join(line[i:i + 80] for i in range(0, len(line), 80)))
-#                 print(writeME)
-#             else:
-#                 writeME = writeME + line
-#         outfile.write(writeME)

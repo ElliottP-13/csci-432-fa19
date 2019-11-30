ecf46994e3d817cd84282fa644fb9a430c29746a
Elliott Pryor <pryorelliott@gmail.com>
2019-11-30 14:53:16 -0700

    Comments

--- shorClassical.py
+++ shorClassical.py
@@ -1,14 +1,14 @@
 import random
 
 
-def power(a, b):
+def power(a, b):  # To avoid overflow error, we calculate the 'slow way'
     z = 1
     for i in range(int(b)):
         z *= a
     return z
 
 
-def euclid(a, b):
+def euclid(a, b):  # Implements Euclid's algorithm for GCD
     while a != 0:
         temp = a
         a = b % a
@@ -16,26 +16,26 @@ def euclid(a, b):
     return b
 
 
-def findPeriod(g, N):
+def findPeriod(g, N):  # Finds the period of f(x)= g^x % N
     i = 0
-    repeat = (g**(i)) % N
+    repeat = (g**(i)) % N  # The number we are looking for
     r = -1
-    z = g**i
-    while r != repeat:
+    z = g**i  # the current number we are checking, ie. g^x
+    while r != repeat:  # until we find the number, keep increasing x
         i += 1
-        z *= g
-        # r = (g**(i + 1)) % N
+        z *= g  # increments the exponent: z = g^i --> z = g^(i+1)
         r = z % N
     return i
 
 
-p = -1
-N = 314191
+p = -1  # initializes our variables
+N = 314191  # the number we are factoring
 g = 2
 while p % 2 != 0:
-    g = random.randint(3, N-2)
+    g = random.randint(3, N-2)  # guess a random number
+                                # the range limits the factors to not be 1 or N
 
-    if euclid(g, N) == 1:
+    if euclid(g, N) == 1:  # if we didn't guess a factor improve our guess
         print("Finding period")
         p = findPeriod(g, N)
         print("WE FOUND: " + str(p))
@@ -43,8 +43,8 @@ while p % 2 != 0:
         print("we guessed a factor")
         break
 
-if p == -1:
-    a = float(euclid(g, N))
+if p == -1:  # We guessed a factor so p was never updated
+    a = float(euclid(g, N)) # We float them just so they are consistent
     b = float(N / a)
 else:
     print("PERIOD: " + str(p))

b00c93048793f9f43ea14dd78ea4d1266315e3d6
Elliott Pryor <pryorelliott@gmail.com>
2019-11-30 14:46:49 -0700

    Period Finding Works

--- shorClassical.py
+++ shorClassical.py
@@ -1,30 +1,44 @@
 import random
-import math
+
+
+def power(a, b):
+    z = 1
+    for i in range(int(b)):
+        z *= a
+    return z
+
 
 def euclid(a, b):
-    if a == 0:
-        return b
-
-    return euclid(b % a, a)
+    while a != 0:
+        temp = a
+        a = b % a
+        b = temp
+    return b
 
 
 def findPeriod(g, N):
-    i = 1
-    repeat = math.pow(g, i) % N
+    i = 0
+    repeat = (g**(i)) % N
     r = -1
+    z = g**i
     while r != repeat:
         i += 1
-        r = g**(i + 1) % N
+        z *= g
+        # r = (g**(i + 1)) % N
+        r = z % N
     return i
 
+
 p = -1
-N = 21
+N = 314191
 g = 2
 while p % 2 != 0:
     g = random.randint(3, N-2)
 
     if euclid(g, N) == 1:
+        print("Finding period")
         p = findPeriod(g, N)
+        print("WE FOUND: " + str(p))
     else:
         print("we guessed a factor")
         break
@@ -35,8 +49,9 @@ if p == -1:
 else:
     print("PERIOD: " + str(p))
     print("GUESS: " + str(g))
-    a = euclid(g ** (p/2) + 1, N)
-    b = euclid(g ** (p/2) - 1, N)
+    num = power(g, p/2)
+    a = euclid(num + 1, N)
+    b = euclid(num - 1, N)
 
 print(a)
 print(b)
